e7600e5b34c9f19f887fa88365cc996c5169c324
namsanghyeok <artexi1464.2@gmail.com>
2024-06-20 19:25:04 +0900

    [#58] feat: transaction id log 남김

--- src/main/java/com/sparta/spartime/web/config/WebConfig.java
+++ src/main/java/com/sparta/spartime/web/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.sparta.spartime.web.config;
 
 import com.sparta.spartime.web.argumentResolver.LoginUser;
+import com.sparta.spartime.web.interceptor.transaction.TransactionIdInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUser());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TransactionIdInterceptor())
+                .addPathPatterns("/**");
     }
 }
