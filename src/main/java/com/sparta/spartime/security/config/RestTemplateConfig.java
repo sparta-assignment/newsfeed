7c07f60846cad9ed577f766c99c6f2cb4c0ac132
Han <nuher038@gmail.com>
2024-06-21 17:11:24 +0900

    Spartime-10 소셜로그인

---
+++ src/main/java/com/sparta/spartime/security/config/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.sparta.spartime.security.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig  {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setReadTimeout(Duration.ofSeconds(3))
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .build();
+    }
+}
