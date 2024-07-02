c9329a2f661763514eda1fef0e2b4da9ad50f8a5
hyun <hyun1202a@gmail.com>
2024-07-02 17:21:19 +0900

    add: querydsl 추가

--- src/main/java/com/sparta/spartime/repository/PostRepository.java
+++ src/main/java/com/sparta/spartime/repository/PostRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
 
     Page<Post> findByType(Post.Type type, Pageable pageable);
 }
